98a888b2d645c5e2229851d3d5d2354e23c9d929
okei@keio.jp <glriefaet@icloud.com>
2025-08-10 15:01:44 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
-// ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL =
+  "https://okei-okei.github.io/tech-blog.github.io/top.html#";
